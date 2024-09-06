437bacc7e6e171dd5940b11c1d340d5f6ec8b65b
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-09-05 20:26:41 -0400

    redraw grid on container resize

--- app/components/GraphMouseWarp.tsx
+++ app/components/GraphMouseWarp.tsx
@@ -1,49 +1,88 @@
 import * as React from 'react'
 import { range } from 'lodash-es'
 
+// on mouse move adjust path lines
+
 const SVG_NAMESPACE = 'http://www.w3.org/2000/svg'
 const GRID_SIZE = 20
 
-function Line({
-  x,
-  y,
-  width,
-  height,
-}: {
-  x: number
-  y: number
-  width: number
-  height: number
-}) {
+type Axis = 'row' | 'col'
+type Coordinate = { x: number; y: number }
+
+type LineProps = Coordinate & { size: number; axis: Axis }
+function Line({ x, y, size, axis }: LineProps) {
   const line = document.createElementNS(SVG_NAMESPACE, 'path')
-  line.setAttribute('d', `M ${x} ${y} Q ${x},${y} ${width},${height}`)
+
+  const end = {
+    row: { x: size, y },
+    col: { x, y: size },
+  }
+
+  line.setAttribute(
+    'd',
+    `M ${x} ${y} Q ${x},${y} ${end[axis].x},${end[axis].y}`
+  )
+  line.setAttribute('class', axis)
 
   return line
 }
 
+function calcGridFromRect({ width, height }: DOMRect) {
+  return {
+    rows: Math.ceil(height / GRID_SIZE),
+    cols: Math.ceil(width / GRID_SIZE),
+  }
+}
+
 export function GraphMouseWarp() {
   const containerRef = React.useRef<SVGSVGElement>(null)
+  const observer = React.useRef<ResizeObserver>()
 
-  // on mount draw initial lines
+  function drawGrid(rect: DOMRect) {
+    const { rows, cols } = calcGridFromRect(rect)
+
+    // draw rows
+    for (const row of range(0, rows)) {
+      const y = row * GRID_SIZE
+      const line = Line({
+        x: 0,
+        y,
+        size: rect.width,
+        axis: 'row',
+      })
+      containerRef.current?.appendChild(line)
+    }
+
+    // draw cols
+    for (const col of range(0, cols)) {
+      const x = col * GRID_SIZE
+      const line = Line({
+        x,
+        y: 0,
+        size: rect.height,
+        axis: 'col',
+      })
+      containerRef.current?.appendChild(line)
+    }
+  }
+
+  // on mount draw initial grid lines
   React.useLayoutEffect(() => {
     if (containerRef.current) {
-      const { width, height } =
-        containerRef.current?.getBoundingClientRect()
+      drawGrid(containerRef.current.getBoundingClientRect())
 
-      // Append horizontal lines
-      const rows = Math.ceil(height / GRID_SIZE)
-      for (const row of range(0, rows)) {
-        const y = row * GRID_SIZE
-        const line = Line({ x: 0, y, width, height: y })
-        containerRef.current.appendChild(line)
-      }
+      // Observe container resize to add/remove grid lines
+      observer.current = new ResizeObserver((entries) => {
+        for (const entry of entries) {
+          drawGrid(entry.contentRect)
+        }
+      })
 
-      // Append vertical lines
-      const cols = Math.ceil(width / GRID_SIZE)
-      for (const col of range(0, cols)) {
-        const x = col * GRID_SIZE
-        const line = Line({ x, y: 0, width: x, height })
-        containerRef.current.appendChild(line)
+      observer.current.observe(containerRef.current)
+
+      return () => {
+        containerRef.current?.replaceChildren()
+        observer.current?.disconnect()
       }
     }
   }, [])
